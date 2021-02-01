88aa67e96c15cb7acb0e0aa11451427d2dba613e
ishu22ji <78218217+ishu22ji@users.noreply.github.com>
2021-02-01 03:44:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 const mineflayer = require('mineflayer')
+const cmd = require('mineflayer-cmd').plugin
+const fs = require('fs');
+let rawdata = fs.readFileSync('config.json');
+let data = JSON.parse(rawdata);
+var lasttime = -1;
+var moving = 0;
+var connected = 0;
+var actions = [ 'forward', 'back', 'left', 'right']
+var lastaction;
+var pi = 3.14159;
+var moveinterval = 2; // 2 second movement interval
+var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+var host = data["ip"];
+var username = data["name"]
+var nightskip = data["auto-night-skip"]
+const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
 const GoalFollow = goals.GoalFollow
 const GoalBlock = goals.GoalBlock
+var bot = mineflayer.createBot({
+  host: host,
+  username: username
+});
+function getRandomArbitrary(min, max) {
+       return Math.random() * (max - min) + min;
 
-const bot = mineflayer.createBot({
-    host: 'hloji.exaroton.me',
-    port: 57763,
-    username: 'ishu_ka_bodyguard'
-})
-
-bot.loadPlugin(pathfinder)
-
-bot.loadPlugin(pathfinder)
-
-function followPlayer() {
-    const playerCI = bot.players['ishubro']
-
-    if (!playerCI || !playerCI.entity) {
-        bot.chat("I can't see CI!")
-        return
-    }
-
-    const mcData = require('minecraft-data')(bot.version)
-    const movements = new Movements(bot, mcData)
-    movements.scafoldingBlocks = []
-
-    bot.pathfinder.setMovements(movements)
-
-    const goal = new GoalFollow(playerCI.entity, 1)
-    bot.pathfinder.setGoal(goal, true)
 }
+bot.loadPlugin(pathfinder)
+bot.loadPlugin(cmd)
 
 
-bot.once('spawn', followPlayer)
+
+bot.on('login',function(){
+	console.log("Logged In")
+	bot.chat("hlo");
+});
+bot.on('chat', (username, message) => {
+  if (message === 'follow') {
+         const playerCI = bot.players['ishubro']
+
+         if (!playerCI || !playerCI.entity) {
+             bot.chat("I can't see CI!")
+             return
+         }
+
+         const mcData = require('minecraft-data')(bot.version)
+         const movements = new Movements(bot, mcData)
+         movements.scafoldingBlocks = []
+
+         bot.pathfinder.setMovements(movements)
+
+         const goal = new GoalFollow(playerCI.entity, 1)
+         bot.pathfinder.setGoal(goal, true)
+  }
+
 
 
 
