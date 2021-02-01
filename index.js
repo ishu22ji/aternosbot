ad354b5332a31cb0edc55ab34fc06b419a42d2a6
ishu22ji <78218217+ishu22ji@users.noreply.github.com>
2021-02-01 03:13:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -23,52 +23,31 @@ function getRandomArbitrary(min, max) {
 
 }
 
-bot.loadPlugin(cmd)
+bot.loadPlugin(pathfinder)
 
+bot.loadPlugin(pathfinder)
 
+function followPlayer() {
+    const playerCI = bot.players['ishubro']
 
-bot.on('login',function(){
-	console.log("Logged In")
-	bot.chat("/login qwe123!");
-});
-
-bot.on('time', function(time) {
-	if(nightskip == "true"){
-	if(bot.time.timeOfDay >= 13000){
-	bot.chat('/time set day')
-	}}
-    if (connected <1) {
-        return;
+    if (!playerCI || !playerCI.entity) {
+        bot.chat("I can't see CI!")
+        return
     }
-    if (lasttime<0) {
-        lasttime = bot.time.age;
-    } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction,false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
-                bot.look(yaw,pitch,false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
-            }
-        }
-    }
-});
 
-bot.on('spawn',function() {
-    connected=1;
-});
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = []
 
-bot.on('death',function() {
-    bot.emit("respawn")
-});
+    bot.pathfinder.setMovements(movements)
+
+    const goal = new GoalFollow(playerCI.entity, 1)
+    bot.pathfinder.setGoal(goal, true)
+}
+
+
+bot.once('spawn', followPlaye)
+
+
+
 
